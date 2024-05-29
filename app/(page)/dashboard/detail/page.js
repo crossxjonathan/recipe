c9bf158f9e96da7e8ef41e26f2a943b49ab5e388
crossxjonathan <crossxjonathan@gmail.com>
2024-05-29 15:37:25 +0700

    rename detail

--- app/(page)/dashboard/detail/page.js
+++ app/(page)/dashboard/detail/page.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import DetailRecipe from './detailRecipe';
 import Loading from './loadingrecipe';
 
+
 const DetailRecipePage = () => {
   return (
     <Suspense fallback={<Loading />}>
